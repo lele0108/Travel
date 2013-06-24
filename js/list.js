b318127b098d502eaab069d878be897b1a4ecd52
michalm244 <michaelm244@gmail.com>
2013-06-24 12:18:16 -0700

    I NEED HELP PARSING THE JSON

--- js/list.js
+++ js/list.js
@@ -10,8 +10,8 @@ $(document).ready(function(){
 	//this contains the query
 	var duration = params.duration;
 	var duration_text = decodeURI(duration);
-	var city = params.location;
-	var city_text = decodeURI(location);
+	var city = params.city;
+	var city_text = decodeURI(city);
 	var location = params.location;
 	var location_text = decodeURI(location);
 
@@ -23,7 +23,7 @@ $(document).ready(function(){
 	var html_insert = '';
 
 	MyModel = Backbone.Model.extend({
-		urlRoot: "http://blankket-mk8te7kbzv.elasticbeanstalk.com/incoming?location=Louise%20M.%20Davies%20Symphony%20Hall(San%20Francisco)&duration=5",
+		urlRoot: "",
 		defaults:{
 			// set defaults here!
 		},
@@ -32,17 +32,20 @@ $(document).ready(function(){
 		}
 	});
 	var the_model = new MyModel();
-	the_model.url = 'http://blankket-mk8te7kbzv.elasticbeanstalk.com/incoming?location='+location+'&duration='+duration;
+	the_model.url = 'http://blankket-mk8te7kbzv.elasticbeanstalk.com/getroutes?city='+city+'&duration='+duration+'&location='+location;
 	the_model.fetch({
 		success: function(response){
 			JSON_RESPONSE = response.toJSON();
-			best_trip = JSON_RESPONSE[0];
+			if(JSON_RESPONSE[0] != null)
+				best_trip = JSON_RESPONSE[0];
+			
 			console.log(best_trip);
+			console.log("length of array is: "+best_trip.place.length)
 
 			var size = 0;
-			for (var key in best_trip)
+			for (var key in best_trip.place)
 			{
-				if(best_trip.hasOwnProperty(key))
+				if(best_trip.place.hasOwnProperty(key))
 					size++;
 			} 
 
@@ -50,7 +53,7 @@ $(document).ready(function(){
 			for (var index = 0; index < size; index++)
 			{
 				var place = best_trip.place[index];
-				html_insert += "<p>Place Name"+place.placeName+"</p>";
+				html_insert += "<p>Place Name: "+place.placeName+"</p>";
 			}
 			$("#content").html(html_insert);
 
